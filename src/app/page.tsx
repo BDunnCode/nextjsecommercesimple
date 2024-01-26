1383c7d1bb4f6c98bef8fbc550231108940f14f3
BDunnCode <BDunnCode@github.com>
2024-01-26 14:18:32 -0600

    minimalistic ui complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+
+import Cart from "@/components/Cart";
+import Product from "@/components/Product";
 import Image from "next/image";
 
-const products: any = [
+const products: Product[] = [
   {
     id: "1",
     name: "GoPro",
@@ -24,7 +27,15 @@ const products: any = [
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
+      <div className="flex flex-col gap-8">
+        <h1 className="text-3xl">E-Commerce Cart System</h1>
+        <div className="grid grid-cols-3 gap-4">
+          {products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+          </div>
+          <Cart />
+      </div>
     </main>
   );
 }
