a4dec0606eabfd39b0cbc7069bc582d2f76302b0
BDunnCode <BDunnCode@github.com>
2024-01-28 14:35:01 -0600

    implementing post request

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -19,7 +19,15 @@ const Cart = () => {
     }
   };
 
-const checkout = async () => {};
+const checkout = async () => {
+  await fetch("http://localhost:3000/api/checkout", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({products: cart})
+  })
+};
 
 
 
